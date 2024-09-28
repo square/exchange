8473cbec15c8147254b90b3cde6d49f9809bb449
Adrian Cole <adrian.cole@elastic.co>
2024-09-28 14:54:43 +1000

    test: refactor OpenAI tests to reduce redundancy
    This refactors the OpenAI tests to reduce copy/paste redundancy.
    Specifically, this changes to parameterized tests, with some
    extra special-case handling for VCR. As a result, was are able
    to have all the openai test deviations in the same file, but not
    a whole lot of scanning to figure out what's different.
    
    Signed-off-by: Adrian Cole <adrian.cole@elastic.co>

--- tests/providers/conftest.py
+++ tests/providers/conftest.py
@@ -1,20 +1,59 @@
 import os
 import re
-from typing import Type, Tuple
+from typing import Type, Tuple, Callable, Dict, Any
 
 import pytest
 
-from exchange import Message, ToolUse, ToolResult, Tool
-from exchange.providers import Usage, Provider
-from tests.conftest import read_file
+from exchange.providers import Provider, AzureProvider, OpenAiProvider
+from exchange.providers.ollama import OllamaProvider
+
+
+def mark_parametrized(providers: Tuple[Type[Provider], str], expected_params: Dict[Type[Provider], Tuple[Any]] = None):
+    """When expected_params is present, we assume it is a VCR test which needs
+    to fake ENV variables to avoid validation failures initializing the
+    provider. This is done via the provider_cls function"""
+
+    def decorator(func: Callable):
+        env_func_name = provider_cls.__name__
+        # Create ids based on provider class names in lowercase without 'Provider' suffix
+        ids = [cls.__name__.replace("Provider", "").lower() for cls, _ in providers]
+        if expected_params is None:
+            return pytest.mark.parametrize("provider_cls,model", providers, ids=ids)(func)
+
+        # Extract expected values based on provider_cls
+        def inner(provider_cls: Type[Provider], model: str) -> Tuple[Any]:
+            expected_values = expected_params.get(provider_cls)
+            if expected_values is None:
+                raise ValueError(f"No expectations found for provider class: {provider_cls}")
+            return func(provider_cls, model, *expected_values)
+
+        return pytest.mark.parametrize("provider_cls,model", providers, ids=ids, indirect=[env_func_name])(inner)
+
+    return decorator
+
+
+@pytest.fixture(scope="function")
+def provider_cls(request, monkeypatch):
+    """Returns the appropriate environment setup based on the provider class."""
+    provider_cls = request.param
+    if provider_cls == AzureProvider:
+        default_azure_env(monkeypatch)
+        return provider_cls
+    elif provider_cls == OllamaProvider:
+        return provider_cls
+    elif provider_cls == OpenAiProvider:
+        default_openai_env(monkeypatch)
+        return provider_cls
+    else:
+        raise ValueError(f"Unexpected provider: {provider_cls}")
+
 
 OPENAI_API_KEY = "test_openai_api_key"
 OPENAI_ORG_ID = "test_openai_org_key"
 OPENAI_PROJECT_ID = "test_openai_project_id"
 
 
-@pytest.fixture
-def default_openai_env(monkeypatch):
+def default_openai_env(monkeypatch: pytest.MonkeyPatch) -> None:
     """
     This fixture prevents OpenAIProvider.from_env() from erring on missing
     environment variables.
@@ -33,8 +72,7 @@ AZURE_API_VERSION = "2024-05-01-preview"
 AZURE_API_KEY = "test_azure_api_key"
 
 
-@pytest.fixture
-def default_azure_env(monkeypatch):
+def default_azure_env(monkeypatch: pytest.MonkeyPatch) -> None:
     """
     This fixture prevents AzureProvider.from_env() from erring on missing
     environment variables.
@@ -96,34 +134,3 @@ def scrub_response_headers(response):
     response["headers"]["openai-organization"] = OPENAI_ORG_ID
     response["headers"]["Set-Cookie"] = "test_set_cookie"
     return response
-
-
-def complete(provider_cls: Type[Provider], model: str) -> Tuple[Message, Usage]:
-    provider = provider_cls.from_env()
-    system = "You are a helpful assistant."
-    messages = [Message.user("Hello")]
-    return provider.complete(model=model, system=system, messages=messages, tools=None)
-
-
-def tools(provider_cls: Type[Provider], model: str) -> Tuple[Message, Usage]:
-    provider = provider_cls.from_env()
-    system = "You are a helpful assistant. Expect to need to read a file using read_file."
-    messages = [Message.user("What are the contents of this file? test.txt")]
-    return provider.complete(model=model, system=system, messages=messages, tools=(Tool.from_function(read_file),))
-
-
-def vision(provider_cls: Type[Provider], model: str) -> Tuple[Message, Usage]:
-    provider = provider_cls.from_env()
-    system = "You are a helpful assistant."
-    messages = [
-        Message.user("What does the first entry in the menu say?"),
-        Message(
-            role="assistant",
-            content=[ToolUse(id="xyz", name="screenshot", parameters={})],
-        ),
-        Message(
-            role="user",
-            content=[ToolResult(tool_use_id="xyz", output='"image:tests/test_image.png"')],
-        ),
-    ]
-    return provider.complete(model=model, system=system, messages=messages, tools=None)
--- tests/providers/test_openai.py
+++ tests/providers/test_openai.py
@@ -2,62 +2,160 @@ import os
 
 import pytest
 
-from exchange import Text, ToolUse
-from exchange.providers.openai import OpenAiProvider
-from .conftest import complete, vision, tools
+from exchange import Text, Message, ToolUse, ToolResult, Tool
+from exchange.providers import OpenAiProvider, AzureProvider
+from exchange.providers.ollama import OllamaProvider, OLLAMA_MODEL
+from .conftest import mark_parametrized
+from ..conftest import read_file
 
-OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
+
+# provider configuration for all tests except vision
+providers = [
+    (OllamaProvider, os.getenv("OLLAMA_MODEL", OLLAMA_MODEL)),
+    (OpenAiProvider, os.getenv("OPENAI_MODEL", "gpt-4o-mini")),
+    (AzureProvider, os.getenv("AZURE_MODEL", "gpt-4o-mini")),
+]
+
+# Currently, only OpenAI (not yet Azure) supports vision in its model.
+vision_providers = [t for t in providers if t[0] == OpenAiProvider]
 
 
-@pytest.mark.vcr()
-def test_openai_complete(default_openai_env):
-    reply_message, reply_usage = complete(OpenAiProvider, OPENAI_MODEL)
+@pytest.mark.vcr
+@mark_parametrized(
+    providers,
+    {
+        AzureProvider: ("Hello! How can I assist you today?", 27),
+        OllamaProvider: ("Hello! I'm here to help. How can I assist you today? Let's chat. 😊", 33),
+        OpenAiProvider: ("Hello! How can I assist you today?", 27),
+    },
+)
+def test_complete(provider_cls, model, expected_text: str, expected_total_tokens: int):
+    reply_message, reply_usage = complete(provider_cls, model)
 
-    assert reply_message.content == [Text(text="Hello! How can I assist you today?")]
-    assert reply_usage.total_tokens == 27
+    text = reply_message.content[0]
+    assert isinstance(text, Text), f"Expected Text, but was {type(text).__name__}"
+    assert text.text == expected_text
+    assert reply_usage.total_tokens == expected_total_tokens
 
 
 @pytest.mark.integration
-def test_openai_complete_integration():
-    reply = complete(OpenAiProvider, OPENAI_MODEL)
+@mark_parametrized(providers)
+def test_complete_integration(provider_cls, model):
+    reply_message, reply_usage = complete(provider_cls, model)
 
-    assert reply[0].content is not None
-    print("Completion content from OpenAI:", reply[0].content)
+    text = reply_message.content[0]
+    assert isinstance(text, Text), f"Expected Text, but was {type(text).__name__}"
+    assert text.text is not None
+    assert reply_usage.total_tokens > 0
 
 
-@pytest.mark.vcr()
-def test_openai_tools(default_openai_env):
-    reply_message, reply_usage = tools(OpenAiProvider, OPENAI_MODEL)
+def complete(provider_cls, model):
+    provider = provider_cls.from_env()
+    system = "You are a helpful assistant."
+    messages = [Message.user("Hello")]
+    return provider.complete(
+        model=model,
+        system=system,
+        messages=messages,
+        tools=(),
+        seed=3,
+        temperature=0.1,  # Always set seed and temperature for determinism
+    )
+
+
+@pytest.mark.vcr
+@mark_parametrized(
+    providers,
+    {
+        AzureProvider: ("call_a47abadDxlGKIWjvYYvGVAHa", 125),
+        OllamaProvider: ("call_d14omgr7", 133),
+        OpenAiProvider: ("call_xXYlw4A7Ud1qtCopuK5gEJrP", 122),
+    },
+)
+def test_complete_tools(provider_cls, model, expected_tool_use_id, expected_total_tokens):
+    reply_message, reply_usage = complete_tools(provider_cls, model)
 
     tool_use = reply_message.content[0]
     assert isinstance(tool_use, ToolUse), f"Expected ToolUse, but was {type(tool_use).__name__}"
-    assert tool_use.id == "call_xXYlw4A7Ud1qtCopuK5gEJrP"
+    assert tool_use.id == expected_tool_use_id
     assert tool_use.name == "read_file"
     assert tool_use.parameters == {"filename": "test.txt"}
-    assert reply_usage.total_tokens == 122
+    assert reply_usage.total_tokens == expected_total_tokens
 
 
 @pytest.mark.integration
-def test_openai_tools_integration():
-    reply = tools(OpenAiProvider, OPENAI_MODEL)
+@mark_parametrized(providers)
+def test_complete_tools_integration(provider_cls, model):
+    reply_message, reply_usage = complete_tools(provider_cls, model)
 
-    tool_use = reply[0].content[0]
+    tool_use = reply_message.content[0]
     assert isinstance(tool_use, ToolUse), f"Expected ToolUse, but was {type(tool_use).__name__}"
     assert tool_use.id is not None
     assert tool_use.name == "read_file"
     assert tool_use.parameters == {"filename": "test.txt"}
+    assert reply_usage.total_tokens > 0
 
 
-@pytest.mark.vcr()
-def test_openai_vision(default_openai_env):
-    reply_message, reply_usage = vision(OpenAiProvider, OPENAI_MODEL)
+def complete_tools(provider_cls, model):
+    system = "You are a helpful assistant. Expect to need to read a file using read_file."
+    messages = [Message.user("What are the contents of this file? test.txt")]
+    provider = provider_cls.from_env()
+    return provider.complete(
+        model=model,
+        system=system,
+        messages=messages,
+        tools=(Tool.from_function(read_file),),
+        seed=3,
+        temperature=0.1,  # Always set seed and temperature for determinism
+    )
 
-    assert reply_message.content == [Text(text='The first entry in the menu says "Ask Goose."')]
-    assert reply_usage.total_tokens == 14241
+
+@pytest.mark.vcr
+@mark_parametrized(
+    vision_providers,
+    {
+        OpenAiProvider: ('The first entry in the menu says "Ask Goose."', 14241),
+    },
+)
+def test_complete_vision(provider_cls, model, expected_text, expected_total_tokens):
+    reply_message, reply_usage = complete_vision(provider_cls, model)
+
+    text = reply_message.content[0]
+    assert isinstance(text, Text), f"Expected Text, but was {type(text).__name__}"
+    assert text.text == expected_text
+    assert reply_usage.total_tokens == expected_total_tokens
 
 
 @pytest.mark.integration
-def test_openai_vision_integration():
-    reply = vision(OpenAiProvider, OPENAI_MODEL)
+@mark_parametrized(vision_providers)
+def test_complete_vision_integration(provider_cls, model):
+    reply_message, reply_usage = complete_vision(provider_cls, model)
 
-    assert "ask goose" in reply[0].text.lower()
+    text = reply_message.content[0]
+    assert isinstance(text, Text), f"Expected Text, but was {type(text).__name__}"
+    assert "ask goose" in text.text.lower()
+    assert reply_usage.total_tokens > 0
+
+
+def complete_vision(provider_cls, model):
+    system = "You are a helpful assistant."
+    messages = [
+        Message.user("What does the first entry in the menu say?"),
+        Message(
+            role="assistant",
+            content=[ToolUse(id="xyz", name="screenshot", parameters={})],
+        ),
+        Message(
+            role="user",
+            content=[ToolResult(tool_use_id="xyz", output='"image:tests/test_image.png"')],
+        ),
+    ]
+    provider = provider_cls.from_env()
+    return provider.complete(
+        model=model,
+        system=system,
+        messages=messages,
+        tools=(),
+        seed=3,  # Always set seed and temperature for determinism
+        temperature=0.1,
+    )
